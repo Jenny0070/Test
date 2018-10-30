269f23abce9a34e5b3a6333fb8bfddfea99916d2
Your Name <email@example.com>
2018-10-30 17:07:27 +0800

    add  page

--- src/java/com.bluemsun/entity/Page.java
+++ src/java/com.bluemsun/entity/Page.java
@@ -2,47 +2,88 @@ package com.bluemsun.entity;
 
 import java.util.List;
 
-public class Page<T> {
-	//可获得数据
-	private int pageNum;//当前页,从请求那边传过来。
-	private int pageSize;//每页显示的数据条数。
-	private Long totalRecord;//总的记录条数。查询数据库得到的数据
-	List<T> list;
-	//需要计算数据
-	private long totalPage;//总页数，通过totalRecord和pageSize计算可以得来
-	private int startIndex;//开始索引，也就是我们在数据库中要从第几行数据开始拿，有了startIndex和pageSize， 就知道了limit语句的两个数据，就能获得每页需要显示的数据了
-	private long start;
-	private long end;//分页显示的页数,比如在页面上显示1，2，3，4，5页，start就为1，end就为5，这个也是算过来的
-	public Page(int pageNum,int pageSize,Long totalRecord){
-		this.pageNum=pageNum;
-		this.pageSize=pageSize;
-		this.totalRecord=totalRecord;
-		//计算totalPage
-		if(totalRecord%pageSize==0){
-			this.totalPage=totalRecord/pageSize;
-		}else {
-			this.totalPage=totalRecord/pageSize+1;
+public class Page<T>{
+	
+	//当前页数,无需计算
+	
+	private int pageNum;
+	
+	//每页的记录数，自己规定，无需计算
+	
+	private int pageSize;
+	
+	//总页数，计算
+	
+	private int totalPage;
+	
+	//数据库总的记录数，无需计算
+	
+	private Long totalRecord;
+	
+	//前端所显示的开始页数，需要计算
+	
+	private int start;
+	
+	//结束页数需要计算
+	
+	private int end;
+	
+	//Page对象
+	
+	private List<T> pageList;
+	
+	//开始索引，即从第几行开始拿需要计算
+	
+	private int startIndex;
+	
+	
+	public Page(int pageNum, int pageSize, Long totalRecord) {
+		this.pageNum = pageNum;
+		this.pageSize = pageSize;
+		this.totalRecord = totalRecord;
+		//计算pageNum
+		
+		if (totalRecord%pageSize==0){
+			this.totalPage= (int) (totalRecord/pageSize);
 		}
-		//计算startIndex
-		this.startIndex=pageNum*pageSize-pageSize;
-		//计算start、end默认是5
-		if(totalPage<=5){
-			this.start=1;
-			this.end=totalPage;
-		}else{
-			this.start=pageNum-2;
-			this.end=pageNum+2;
-			if(this.start<=0){
-				this.start=1;
-				this.end=5;
-			}
-			if(this.end>totalPage){
-				this.end=totalPage;
-				this.start=this.end-5;
-			}
+		else{
+			this.totalPage= (int) (totalRecord/pageSize+1);
 		}
 		
+		//startIndex
+		this.startIndex=pageNum*pageSize-pageSize;
+		
+		//start,end
+		
+		if (totalPage<=7){
+			this.start=1;
+			this.end=pageSize;
+		}
+		else{
+			//一共显示7页
+			
+			this.start=pageNum-3;
+			this.end=pageNum+3;
+			
+			if(this.start<=0){
+				this.start=1;
+				this.end=7;
+			}
+			else if(this.end>totalPage){
+				this.start=totalPage-7;
+				this.end=totalPage;
+			}
+		}
 	}
+	
+	public int getTotalPage() {
+		return totalPage;
+	}
+	
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	
 	
 	public int getPageNum() {
 		return pageNum;
@@ -68,20 +109,28 @@ public class Page<T> {
 		this.totalRecord = totalRecord;
 	}
 	
-	public List<T> getList() {
-		return list;
+	public int getStart() {
+		return start;
 	}
 	
-	public void setList(List<T> list) {
-		this.list = list;
+	public void setStart(int start) {
+		this.start = start;
 	}
 	
-	public Long getTotalPage() {
-		return totalPage;
+	public int getEnd() {
+		return end;
 	}
 	
-	public void setTotalPage(int totalPage) {
-		this.totalPage = totalPage;
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	public List<T> getPageList() {
+		return pageList;
+	}
+	
+	public void setPageList(List<T> pageList) {
+		this.pageList = pageList;
 	}
 	
 	public int getStartIndex() {
@@ -91,24 +140,4 @@ public class Page<T> {
 	public void setStartIndex(int startIndex) {
 		this.startIndex = startIndex;
 	}
-	
-	public Long getStart() {
-		return start;
-	}
-	
-	public void setStart(int start) {
-		this.start = start;
-	}
-	
-	public Long getEnd() {
-		return end;
-	}
-	
-	public void setEnd(int end) {
-		this.end = end;
-	}
-	
-	
-	
 }
-
